403f9058af81236c67ccb7dbfa7a8400f28a7281
Peter Hormanns <peter.hormanns@jalin.de>
2014-12-03 17:28:13 +0100

    main class

---
+++ src/main/java/net/hostsharing/admin/example/Main.java
@@ -0,0 +1,31 @@
+package net.hostsharing.admin.example;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+
+public class Main {
+
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+		try {
+			final XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+			config.setServerURL(new URL("https://config.hostsharing.net:443/hsar/xmlrpc/hsadmin"));
+			config.setEnabledForExtensions(true);
+			final XmlRpcClient client = new XmlRpcClient();
+			client.setConfig(config);
+			client.execute("", new ArrayList());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XmlRpcException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
